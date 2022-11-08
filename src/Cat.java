09b750aa2d174e46ee7ea51dfc56725df5f26eab
lesson1 <kubanmuit@gmail.com>
2022-11-08 15:00:17 +0600

    commit

--- src/Cat.java
+++
@@ -1,6 +0,0 @@
-public class Cat extends Animals{
-
-    public Cat(String name, String color, int age) {
-        super(name, color, age);
-    }
-}
--- src/Dog.java
+++
@@ -1,8 +0,0 @@
-public class Dog extends Animals{
-
-
-    public Dog(String name, String color, int age) {
-        super(name, color, age);
-    }
-
-}
